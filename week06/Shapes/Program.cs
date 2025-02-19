2050fb05426156e98c05aea70613fedb86fd5da0
SamuelSoriano80 <samuelsoriano80@gmail.com>
2025-02-18 22:01:53 -0600

    Polymorphism done

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square shape1 = new Square("Blue", 5);
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle("Green", 2, 3);
+        shapes.Add(shape2);
+
+        Circle shape3 = new Circle("Red", 4);
+        shapes.Add(shape3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The area of the {color} shape is {area}.");
+        }
     }
 }
